72590e7236ddef08b9768b35bf55bf1abd803856
Matt Dawson <mattdawson18@gmail.com>
2021-07-15 15:43:06 +0100

    Conditional control flow makes for better user friendliness

--- app1.py
+++ app1.py
@@ -8,7 +8,13 @@ def translate(w):
     if w in data:
         return data[w]
     elif len(get_close_matches(w, data.keys(), cutoff=0.8)) > 0:
-        return "Did you mean %s instead?" % get_close_matches(w, data.keys(), cutoff=0.8)[0]
+        response = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys(), cutoff=0.8)[0])
+        if response ==  "Y" or response == "y":
+            return data[get_close_matches(w, data.keys())[0]]
+        elif response == "N" or response == "n":
+            return "The word does not exist. Please check it."
+        else: 
+            return "We didn't understand your entry"
     else:
         return "The word does not exist. Please check it."
 
