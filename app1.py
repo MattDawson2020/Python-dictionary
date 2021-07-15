998a797c5a076b8913ce3f7562e52c31a8f79cb9
Matt Dawson <mattdawson18@gmail.com>
2021-07-15 15:33:44 +0100

    Now uses difflib library to check for close matches

--- app1.py
+++ app1.py
@@ -1,10 +1,14 @@
 import json
+from difflib import get_close_matches
+
 data = json.load(open("data.json"))
 
 def translate(w):
     w = w.lower()
     if w in data:
         return data[w]
+    elif len(get_close_matches(w, data.keys(), cutoff=0.8)) > 0:
+        return "Did you mean %s instead?" % get_close_matches(w, data.keys(), cutoff=0.8)[0]
     else:
         return "The word does not exist. Please check it."
 
